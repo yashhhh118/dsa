f5da3e4472a6495e1afae83baf1dcbd011d6bc7c
yashhhh18 <yashhhwhat@gmail.com>
2026-01-12 18:37:57 +0530

    age file

---
+++ age.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+ using namespace std;
+ int main() {
+    int age;
+ 
+    cout << "enter your age: ";
+    cin >> age;
+
+    if (age >= 18) {
+        cout << "you are an adult." << endl;
+    } else {
+        cout << "you are not an adult." << endl;
+    }
+    return 0;
+}
+ 
