443a3213e381de77ea8e97031cbee759dcad685d
yashhhh18 <yashhhwhat@gmail.com>
2026-01-12 23:27:51 +0530

    add switchstatment

---
+++ switchstatment.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int day;
+
+    cout << "Enter a number (1-7): ";
+    cin >> day;
+
+    switch (day) {
+        case 1:
+            cout << "Monday" << endl;
+            break; 
+        case 2:
+            cout << "Tuesday" << endl;
+            break;
+        case 3:
+            cout << "Wednesday" << endl;
+            break;
+        case 4:
+            cout << "Thursday" << endl;
+            break;
+        case 5:
+            cout << "Friday" << endl;
+            break;
+        case 6:
+            cout << "Saturday" << endl;
+            break;
+        case 7:
+            cout << "Sunday" << endl;
+            break;
+        default:
+            // Executed if input is not between 1 and 7
+            cout << "Invalid" << endl;
+    }
+
+    return 0; // End program
+}
