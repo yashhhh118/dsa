8cc4ff9ebdce227f7c3d9468b195801e60172294
yashhhh18 <yashhhwhat@gmail.com>
2026-01-13 16:51:58 +0530

    new file

--- constant.cpp
+++ constant.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 using namespace std;
 int main() {
-    const int x = 10;
-    const int y  = 7;
-
-    switch (x+y) {
-        case 15:
-        cout << "Result is 15." <<endl;
+    char grade = 'G';
+    switch(grade) {
+        case 'V':
+        cout <<"Excellent" <<endl;
         break;
 
-        case 20:
-        cout << "result is 20" <<endl;
+        case'B':
+        cout <<"good" <<endl;
         break;
-    default:
-        cout << "no match found." <<endl;
-        return 0;
+
+        default:
+        cout <<"not specified."  <<endl;
+
     }
+    return 0;
 }
