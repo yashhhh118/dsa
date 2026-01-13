9bdd2fe94e0a2cf11cd607332690f51fca335839
yashhhh18 <yashhhwhat@gmail.com>
2026-01-13 16:27:28 +0530

    new file constant

---
+++ constant.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    const int x = 10;
+    const int y  = 7;
+
+    switch (x+y) {
+        case 15:
+        cout << "Result is 15." <<endl;
+        break;
+
+        case 20:
+        cout << "result is 20" <<endl;
+        break;
+    default:
+        cout << "no match found." <<endl;
+        return 0;
+
+    }
+}
