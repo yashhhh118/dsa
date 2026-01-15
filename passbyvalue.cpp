5be848b5bc2efad9b9f3a495aaed6c49453fc7d1
yashhhh18 <yashhhwhat@gmail.com>
2026-01-15 20:01:51 +0530

    addition

--- passbyvalue.cpp
+++ passbyvalue.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
 
-void modify(int a) {
+void modify(int &a) {
     a = a + 10;
 }
 
 int main() {
     int x = 5;
     modify(x);
-    cout << x << endl; 
+    cout << x << endl; // Output: 15
     return 0;
 }
