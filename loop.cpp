5d8346b07e3db06d4a5fdf5a76bacc61855ed8cd
yashhhh18 <yashhhwhat@gmail.com>
2026-01-15 20:10:21 +0530

    adddd

--- loop.cpp
+++ loop.cpp
@@ -2,10 +2,9 @@
 using namespace std;
 
 int main() {
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            cout << "i = " << i << ", j = " << j << endl;
-        }
+    for (int i = 1; i <= 25; i += 5) {
+        cout << "i = " << i << endl;
     }
     return 0;
 }
+
