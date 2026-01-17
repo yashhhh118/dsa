ab9487777f56232c00afe1832baf4ee2548e9745
yashhhh18 <yashhhwhat@gmail.com>
2026-01-17 20:02:28 +0530

    subscrition

--- whileloop.cpp
+++ whileloop.cpp
@@ -1,18 +1,19 @@
-#include <iostream>
+#include <bits/stdc++.h>
+
 using namespace std;
 
 int main() {
+    int t;
+    cin>>t;
+    while(t--) {
+        int x,y;
+        cin>>x>>y;
+        if(x%6==0){
+            cout<<(x/6)*x<<endl;
+        }else{
+            cout<<((x/6)+1)*x<<endl;
+        }
+    }
+    return 0;
 
-	int t;
-	cin>>t;
-	while(t--){
-	    int n,x;
-	    cin>>n>>x;
-	    if(n%6==0){
-	        cout<<(n/6)*x<<endl;
-	    }else{
-	        cout<<((n/6)+1)*x<<endl;
-	    }
-	}
-	return 0;
 }
