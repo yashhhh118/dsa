2730efc666d47a9d0e64b3f569f2fef3948cd942
yashhhh18 <yashhhwhat@gmail.com>
2026-01-17 19:28:00 +0530

    whileloopp

---
+++ random.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,x;
+	    cin>>n>>x;
+	    if(n%6==0){
+	        cout<<(n/6)*x<<endl;
+	    }else{
+	        cout<<((n/6)+1)*x<<endl;
+	    }
+	}
+	return 0;
+}
