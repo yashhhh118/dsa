df88364bb83c1b4b12487b61a5caa2d342a9cca6
yashhhh18 <yashhhwhat@gmail.com>
2026-01-20 15:47:24 +0530

    asd

--- himu.cpp
+++ himu.cpp
@@ -7,16 +7,16 @@ int main() {
     cin>>t;
     while(t--){
     
-    int x, y, z;
-    cin >> x>> y>> z;
+        int x, y, z;
+        cin >> x>> y>> z;
 
-    int totalStudents = x * y;
+        int totalStudents = x * y;
 
-    if (z > totalStudents / 2) {
-        cout << "YES"<<endl;
-    } else {
+        if (z > totalStudents / 2) {
+            cout << "YES"<<endl;
+        } else {
         cout << "NO"<<endl;
-    }
+        }
     }
     return 0;
 }
