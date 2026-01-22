ef7b4c22b0962046daf15db0eb01b7da2037834e
yashhhh18 <yashhhwhat@gmail.com>
2026-01-22 23:48:57 +0530

    abs

--- switchstatment.cpp
+++ switchstatment.cpp
@@ -2,37 +2,24 @@
 using namespace std;
 
 int main() {
-    int day;
+    int n;
+    cin >> n;
 
-    cout << "Enter a number (1-7): ";
-    cin >> day;
+    int arr[1000];
 
-    switch (day) {
-        case 1:
-            cout << "Monday" << endl;
-            break; 
-        case 2:
-            cout << "Tuesday" << endl;
-            break;
-        case 3:
-            cout << "Wednesday" << endl;
-            break;
-        case 4:
-            cout << "Thursday" << endl;
-            break;
-        case 5:
-            cout << "Friday" << endl;
-            break;
-        case 6:
-            cout << "Saturday" << endl;
-            break;
-        case 7:
-            cout << "Sunday" << endl;
-            break;
-        default:
-            // Executed if input is not between 1 and 7
-            cout << "Invalid" << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
     }
 
-    return 0; // End program
+    int maxElement = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > maxElement) {
+            maxElement = arr[i];
+        }
+    }
+
+    cout << maxElement;
+
+    return 0;
 }
