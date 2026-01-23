9205ec52810ee1041ad004e3b3c0770818704521
yashhhh18 <yashhhwhat@gmail.com>
2026-01-23 22:30:43 +0530

    addddd

--- sum.cpp
+++ sum.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-    int N;
-    cin >> N;
+    string s;
+    cin >> s;
 
-    int sum = N * (N + 1) / 2;
-
-    cout << sum;
+    for (int i = s.length() - 1; i >= 0; i--) {
+        cout << s[i];
+    }
 
     return 0;
 }
