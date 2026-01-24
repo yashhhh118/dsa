ce8bf0a4a1398e511594ecb42a5f14a108796c3c
yashhhh18 <yashhhwhat@gmail.com>
2026-01-24 23:18:38 +0530

    new one

--- whileloop.cpp
+++ whileloop.cpp
@@ -1,13 +1,29 @@
 #include <iostream>
+#include <climits>
 using namespace std;
 
 int main() {
-	int T,X,Y;
-	cin>>T;
-	while(T--)
-	{
-	    cin>>X>>Y;
-	    cout<<4*X+Y<<endl;
-	}
-	return 0;
+    int n;
+    cin >> n;
+
+    int arr[1000];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int largest = INT_MIN;
+    int secondLargest = INT_MIN;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > largest) {
+            secondLargest = largest;
+            largest = arr[i];
+        } else if (arr[i] > secondLargest && arr[i] != largest) {
+            secondLargest = arr[i];
+        }
+    }
+
+    cout << secondLargest;
+
+    return 0;
 }
