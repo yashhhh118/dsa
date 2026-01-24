b82f6af851a99abf1f17bb8cb2fdb0706de62d90
yashhhh18 <yashhhwhat@gmail.com>
2026-01-24 22:53:45 +0530

    sydlf

--- new.cpp
+++ new.cpp
@@ -1,29 +1,16 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-    int num;
-    cin >> num;
 
-    if (num <= 1) {
-        cout << "NO";
-        return 0;
-    }
-
-    bool isPrime = true;
-
-    for (int i = 2; i * i <= num; i++) {
-        if (num % i == 0) {
-            isPrime = false;
-            break;
+    int start,end;
+    cin>>start>>end;
+    for(int i=start;i<=end;i++)
+    {
+        if(i%2==0)
+        {
+            cout<<i<<endl;
+        
         }
     }
-
-    if (isPrime) {
-        cout << "YES";
-    } else {
-        cout << "NO";
-    }
-
     return 0;
 }
