2d8b5595dfb9d80d9a01bafb3f5f97d4ae6b7422
yashhhh18 <yashhhwhat@gmail.com>
2026-01-24 23:16:46 +0530

    random changes

--- constant.cpp
+++ constant.cpp
@@ -1,19 +1,28 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-    char grade = 'G';
-    switch(grade) {
-        case 'V':
-        cout <<"Excellent" <<endl;
-        break;
+    int n;
+    cin >> n;
 
-        case'B':
-        cout <<"good" <<endl;
-        break;
+    int num;
+    int positive = 0, negative = 0, zero = 0;
 
-        default:
-        cout <<"not specified."  <<endl;
+    for (int i = 0; i < n; i++) {
+        cin >> num;
 
+        if (num > 0) {
+            positive++;
+        } else if (num < 0) {
+            negative++;
+        } else {
+            zero++;
+        }
     }
+
+    cout << "Positive: " << positive << endl;
+    cout << "Negative: " << negative << endl;
+    cout << "Zero: " << zero << endl;
+
     return 0;
 }
