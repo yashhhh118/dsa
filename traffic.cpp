be5f8e232058fd02ccdf5332b4e756184db12ff6
yashhhh18 <yashhhwhat@gmail.com>
2026-01-26 23:09:59 +0530

    sum

--- traffic.cpp
+++ traffic.cpp
@@ -1,15 +1,34 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-    int year;
-    cin >> year;
+    string s1, s2;
+    cin >> s1 >> s2;
 
-    if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
-        cout << "LEAP YEAR";
-    } else {
-        cout << "NOT LEAP YEAR";
+    if (s1.length() != s2.length()) {
+        cout << "NO";
+        return 0;
     }
+
+    int freq[26] = {0};
+
+    for (char c : s1) {
+        freq[c - 'a']++;
+    }
+
+    for (char c : s2) {
+        freq[c - 'a']--;
+    }
+
+    for (int i = 0; i < 26; i++) {
+        if (freq[i] != 0) {
+            cout << "NO";
+            return 0;
+        }
+    }
+
+    cout << "YES";
 
     return 0;
 }
