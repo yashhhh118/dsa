681a78ebd46c56d6963f1dad6c2b9d4e29362f5b
yashhhh18 <yashhhwhat@gmail.com>
2026-01-27 23:07:34 +0530

    addd

--- test.cpp
+++ test.cpp
@@ -2,6 +2,33 @@
 using namespace std;
 
 int main() {
-    cout << "DSA journey started 🚀";
+    int n;
+    cin >> n;
+
+    int arr[1000], prefix[1000];
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Build prefix sum array
+    prefix[0] = arr[0];
+    for (int i = 1; i < n; i++) {
+        prefix[i] = prefix[i - 1] + arr[i];
+    }
+
+    int q;
+    cin >> q;
+
+    while (q--) {
+        int l, r;
+        cin >> l >> r;
+
+        if (l == 0)
+            cout << prefix[r] << endl;
+        else
+            cout << prefix[r] - prefix[l - 1] << endl;
+    }
+
     return 0;
 }
